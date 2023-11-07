d241a2d177ea653fb5f3597c10b6eb0a7173546c
James Horgan <jamesrhorgan@gmail.com>
2023-11-07 10:59:29 +0000

    Airline model created in models.py

--- pilot_posts/models.py
+++ pilot_posts/models.py
@@ -3,10 +3,22 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
+class Airline(models.Model):
+    name = models.CharField(max_length=40, null=False, blank=False)
+    identifier = models.CharField(max_length=3, null=False, blank=False)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
 class Upload(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = CloudinaryField('image', default='placeholder', null=False, blank=False)
     caption = models.TextField(max_length=100, null=False, blank=False)
+    airline = models.ForeignKey(Airline, on_delete=models.SET_NULL, null=True, blank=True)
     flight_number = models.CharField(max_length=10, null=False, blank=False)
     location = models.CharField(max_length=80)
     created_on = models.DateTimeField(auto_now_add=True)
