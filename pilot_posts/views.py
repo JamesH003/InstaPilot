952c0279f1e3225a0b4d90af2119ade07228a0ca
James Horgan <jamesrhorgan@gmail.com>
2023-11-09 16:52:34 +0000

    Add view to upload a post

--- pilot_posts/views.py
+++ pilot_posts/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
-from .models import Upload
+from .models import Airline, Upload
+from .forms import UploadForm
 
 class UploadList(generic.ListView):
     model = Upload
-    queryset = Upload.objects.filter(status=1).order_by('-created_on')
+    queryset = Upload.objects.filter().order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 12
+
+
+# def get_uploads(request):
+#     uploads = Upload.objects.all().order_by('-created_on')
+#     template = 'index.html'
+#     context = {
+#         'upload_list': uploads,
+#     }
+#     return render(request, template, context)
+
+
+def upload_post(request):
+    form = UploadForm(request.POST or None, request.FILES or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.instance.user = request.user
+            form.save()
+            return redirect('home')
+    context = {
+        'form': form
+    }
+    return render(request, 'upload_post.html', context)
+
