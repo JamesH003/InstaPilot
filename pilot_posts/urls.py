2f114de14b914719cda290cbc64947f6dab74b3e
James Horgan <jamesrhorgan@gmail.com>
2023-11-09 13:58:39 +0000

    Create urls for home and upload_post

--- pilot_posts/urls.py
+++ pilot_posts/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
 from . import views
 
+
+urlpatterns = [
+    path('', views.UploadList.as_view(), name='home'),
+    path('upload_post', views.upload_post, name="upload_post"),
