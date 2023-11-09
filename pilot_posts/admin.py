2f3b12883be91a25003c1cfae25fe1f52b292dbe
James Horgan <jamesrhorgan@gmail.com>
2023-11-09 13:57:06 +0000

    Add Airline model to admin.py

--- pilot_posts/admin.py
+++ pilot_posts/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Upload
+from .models import Airline, Upload 
+
+
+@admin.register(Airline)
+class AirlineAdmin(admin.ModelAdmin):
+    list_display = ("name", "identifier")
 
 
 @admin.register(Upload)
 class UploadAdmin(admin.ModelAdmin):
-    list_display = ("user", "flight_number", "location", "image_preview")
+    list_display = ("user", "airline", "flight_number", "location", "image_preview")
     readonly_fields = ("image_preview", )
