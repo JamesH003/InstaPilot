3a658225b645336d2cbea54c1f3c62634138330f
James Horgan <jamesrhorgan@gmail.com>
2023-11-22 18:57:33 +0000

    Fix errors in python code in urls.py

--- pilot_posts/urls.py
+++ pilot_posts/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.UploadList.as_view(), name='home'),
     path('upload_post', views.upload_post, name="upload_post"),
-    path('edit/<int:id>', views.edit_upload, name='edit_upload'), 
-    path('details/<int:id>', views.upload_details, name='upload_details'), 
-    path('delete/<int:id>', views.delete_post, name='delete_post'), 
+    path('edit/<int:id>', views.edit_upload, name='edit_upload'),
+    path('details/<int:id>', views.upload_details, name='upload_details'),
+    path('delete/<int:id>', views.delete_post, name='delete_post'),
 ]
